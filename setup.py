04606560c251b07e2f9dede00d8583809dfb7d26
Alexander Laird <alaird@twilio.com>
2018-12-17 09:46:17 -0800

    no message

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     packages=["pyngrok"],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     install_requires=[
+        "future",
         "requests",
     ],
     description="A Python wrapper for Ngrok.",
