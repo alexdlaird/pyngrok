644ab4e7ac4dd281e7bcf450f0d8cd0b420edcbb
Alex Laird <contact@alexlaird.com>
2020-06-17 11:42:38 -0700

    Version bump.

--- pyngrok/ngrok.py
+++ pyngrok/ngrok.py
@@ -27,7 +27,7 @@ except ImportError:  # pragma: no cover
 
 __author__ = "Alex Laird"
 __copyright__ = "Copyright 2020, Alex Laird"
-__version__ = "4.0.1"
+__version__ = "4.0.2"
 
 logger = logging.getLogger(__name__)
 
