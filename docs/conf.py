1b5c5c3e79dd4947f130a5ddc42ba1d7342b70be
Alex Laird <contact@alexlaird.com>
2020-08-23 22:17:22 -0700

    Adding custom robots.txt.

--- docs/conf.py
+++ docs/conf.py
@@ -102,6 +102,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
+html_extra_path = ["robots.txt"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
