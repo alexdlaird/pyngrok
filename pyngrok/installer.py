056256e8b36ce244b376f31cd43791554f4d9348
rikuru_to <54424613+rikuru-to865@users.noreply.github.com>
2021-10-16 09:31:12 +0900

    Fix ssl error
    Fixed ngrok not being able to download due to ssl error

--- pyngrok/installer.py
+++ pyngrok/installer.py
@@ -8,6 +8,8 @@ import time
 import zipfile
 from http import HTTPStatus
 from urllib.request import urlopen
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 import yaml
 
